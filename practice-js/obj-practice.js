ef2f8121e25fcba250230e5a702a97f96c19b221
Eishun Sakamoto <258444@st.takushoku-u.ac.jp>
2026-06-18 17:01:05 +0900

    JavaScript関連を編集

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,12 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+
+for (let x of campus.buildingD){
+ console.log(x);
+}
+
+for (let y of gakka){
+	console.log(y.name);
+}
